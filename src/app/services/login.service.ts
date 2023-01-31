15791610d3eb75de6b1dace550e4df48080cd991
Erik Brandt <erikdbrandt@gmail.com>
2023-01-31 15:37:12 +0100

    4 working on services for loginTrainer

--- src/app/services/login.service.ts
+++ src/app/services/login.service.ts
@@ -1,21 +1,66 @@
 import { Injectable } from '@angular/core';
+import {Trainer} from "../models/trainer.model";
+import {map, Observable, of, switchMap} from "rxjs";
+import {HttpClient, HttpHeaders} from "@angular/common/http";
+import {environment} from "../../environments/environment";
+
+const {apiTrainers, apiKey} = environment
 
 @Injectable({
   providedIn: 'root'
 })
 export class LoginService {
 
-  constructor() { }
+  //dependency injection
+  constructor(
+    private readonly http: HttpClient
+  ) { }
 
   //Login
 
+  public login(username: string): Observable<Trainer> {
+
+    return this.checkUsername(username)
+      .pipe(
+        switchMap((trainer: Trainer | undefined) => {
+          if (trainer === undefined) {
+            return this.createTrainer(username);
+          }
+          return of (trainer);
+        })
+      )
+  }
 
 
   // check if user exist
 
+  private checkUsername(username : string): Observable<Trainer | undefined> {
+    return this.http.get<Trainer[]>(`${apiTrainers}?username=${username}`)
+      .pipe(
+        // RxJS operator
+    map((response: Trainer[]) => response.pop())
+      )
+  };
 
   // if not exist, create user
 
+  private createTrainer(username: string): Observable<Trainer> {
+    const user = {
+      username,
+      pokemon: []
+    };
+
+    const headers = new HttpHeaders({
+      'Content-Type': 'application/json',
+      'x-api-key': apiKey
+    });
+
+    return this.http.post<Trainer>(apiTrainers, user, {
+      headers});
+
+    //headers -- api key
+    // POST
+  }
 
   // if exist || created user - store user
 
