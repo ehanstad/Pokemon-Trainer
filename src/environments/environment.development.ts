3a7cd9d9bbf48940f9ad7ceecd89fbfa077b4d80
Erik <erik_hanstad@hotmail.com>
2023-02-03 10:28:19 +0100

    Unignore the envrionments

---
+++ src/environments/environment.development.ts
@@ -0,0 +1,6 @@
+export const environment = {
+  production: false,
+  apiTrainers: "https://noroff-assignment-api-production-49c6.up.railway.app/trainers",
+  apiPokemons: "https://pokeapi.co/api/v2/pokemon",
+  apiKey: "7683087a-e696-4ca9-a5bd-d7e1c96e2ad3",
+};
